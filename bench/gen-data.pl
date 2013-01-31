1b4819abd65b6069c8cad84a188b0e82306ae5ea
agentzh (Yichun Zhang) <agentzh@gmail.com>
2013-01-30 16:25:02 -0800

    bench: use bigger data to reduce timing error.

--- bench/gen-data.pl
+++ bench/gen-data.pl
@@ -6,6 +6,6 @@ use warnings;
 my $infile = "abc.txt";
 open my $in, ">$infile" or
     die "Cannot open $infile for writing: $!\n";
-print $in "abccc" x (1024 * 500) . "aaabbccb";
+print $in "abccc" x (1024 * 1024) . "aaabbccb";
 close $in;
 
