bb4119b6cf79c8dfcda632f6b3610386762816f9
Yichun Zhang (agentzh) <agentzh@gmail.com>
2015-06-17 16:25:29 +0800

    updated the C source by bision 3.0.4.

--- src/sregex/sre_yyparser.h
+++ src/sregex/sre_yyparser.h
@@ -1,19 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.7.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
 /* Bison interface for Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
-   
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,13 +26,13 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
 #ifndef YY_SREGEX_YY_SRC_SREGEX_SRE_YYPARSER_H_INCLUDED
 # define YY_SREGEX_YY_SRC_SREGEX_SRE_YYPARSER_H_INCLUDED
-/* Enabling traces.  */
+/* Debug traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 0
 #endif
@@ -40,68 +40,56 @@
 extern int sregex_yydebug;
 #endif
 
-/* Tokens.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     SRE_REGEX_TOKEN_CHAR = 258,
-     SRE_REGEX_TOKEN_EOF = 259,
-     SRE_REGEX_TOKEN_BAD = 260,
-     SRE_REGEX_TOKEN_CQUANT = 261,
-     SRE_REGEX_TOKEN_CHAR_CLASS = 262,
-     SRE_REGEX_TOKEN_ASSERTION = 263
-   };
+  enum yytokentype
+  {
+    SRE_REGEX_TOKEN_CHAR = 258,
+    SRE_REGEX_TOKEN_EOF = 259,
+    SRE_REGEX_TOKEN_BAD = 260,
+    SRE_REGEX_TOKEN_CQUANT = 261,
+    SRE_REGEX_TOKEN_CHAR_CLASS = 262,
+    SRE_REGEX_TOKEN_ASSERTION = 263
+  };
 #endif
 
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
+
+union YYSTYPE
 {
-/* Line 2058 of yacc.c  */
-#line 82 "src/sregex/sre_yyparser.y"
+#line 82 "src/sregex/sre_yyparser.y" /* yacc.c:1915  */
 
     sre_regex_t         *re;
     sre_char             ch;
     sre_uint_t           group;
     sre_regex_cquant_t   cquant;
 
+#line 70 "src/sregex/sre_yyparser.h" /* yacc.c:1915  */
+};
 
-/* Line 2058 of yacc.c  */
-#line 73 "src/sregex/sre_yyparser.h"
-} YYSTYPE;
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
+/* Location type.  */
 #if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
-typedef struct YYLTYPE
+typedef struct YYLTYPE YYLTYPE;
+struct YYLTYPE
 {
   int first_line;
   int first_column;
   int last_line;
   int last_column;
-} YYLTYPE;
-# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+};
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
 
 
-#ifdef YYPARSE_PARAM
-#if defined __STDC__ || defined __cplusplus
-int sregex_yyparse (void *YYPARSE_PARAM);
-#else
-int sregex_yyparse ();
-#endif
-#else /* ! YYPARSE_PARAM */
-#if defined __STDC__ || defined __cplusplus
+
 int sregex_yyparse (sre_pool_t *pool, sre_char **src, sre_uint_t *ncaps, int flags, sre_regex_t **parsed, sre_char **err_pos);
-#else
-int sregex_yyparse ();
-#endif
-#endif /* ! YYPARSE_PARAM */
 
 #endif /* !YY_SREGEX_YY_SRC_SREGEX_SRE_YYPARSER_H_INCLUDED  */
